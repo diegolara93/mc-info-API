4a0bd82185fca58aab5c611115600315aa76a55e
Diego <diegolara93345@gmail.com>
2023-06-19 13:00:31 -0700

    Added ejs and express

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # mc-info-API
 Uses the craftar API, express, ejs, axios and js to display all information about a minecraft account.
 Displays UUID, username, skin, player head, player body and more.
+-- Plan to add mongoDB in order to have a sign up feature where you can leave comments on others profile and +rep or -rep an account.
--- test.js
+++ test.js
@@ -1,19 +1,23 @@
-const getPlayerHead = async (username) => {
-    const res = await axios.get(`https://playerdb.co/api/player/minecraft/${username}`);
-    const UUID =  res.data.data.player.id
-    console.log(UUID)
-    const img = document.querySelector("#face");
-    const img2 = document.querySelector("#face2");
-    const img3 = document.querySelector("#face3");
-    const img4 = document.querySelector("#face4");
-    document.getElementById("username").innerHTML = username;
-    document.getElementById("uuid").innerHTML = UUID;
-    img.src = `https://crafatar.com/avatars/${UUID}`;
-    img2.src = `https://crafatar.com/renders/head/${UUID}`
-    img3.src = `https://crafatar.com/renders/body/${UUID}`
-    img4.src = `https://crafatar.com/skins/${UUID}`
-}
-const btn = document.querySelector("#click2");
-btn.addEventListener("click", () => {
-    getPlayerHead("GGXGang")
+const express = require("express");
+const app = express();
+const path = require('path');
+const axios = require('axios');
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'views')));
+
+
+app.get('/', (req, res) => {
+    res.render('info')
 })
+app.get('/home', (req, res) => {
+    res.render('home')
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.listen(8080, () => {
+    console.log("server is up")
+})
+
